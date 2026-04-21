9e2d466c83be2ef6a9d7d1f71c1028f836d9e7ea
shaperones <86309764+shaperones0@users.noreply.github.com>
2026-04-21 10:13:10 +0300

    template

---
+++ template/shaders/template_MainPS.hlsl
@@ -0,0 +1,60 @@
+cbuffer vars : register(b0)
+{
+    float2 uResolution;
+    float uTime;
+};
+
+//Texture2D tex : register(t0);
+//SamplerState smp : register(s0);
+
+float2 rotate(float2 v, float2 sc) {
+    //sc = (sin, cos)
+    return float2(
+        v.x * sc.y - v.y * sc.x,
+        v.x * sc.x + v.y * sc.y
+    );
+}
+
+float2 rotate(float2 v, float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+
+    return rotate(v, float2(s,c));
+}
+
+float4 mainC(float2 uv : SV_POSITION) : SV_TARGET {
+    //uvs are already normalized
+    float2 uvR = rotate(uv-0.5f, uTime)*1.5;
+    return float4(uvR.x,uvR.y,1.0f,1.0f);
+}
+
+float4 main(float4 uv : SV_POSITION) : SV_TARGET {
+    return mainC(uv.xy / uResolution);
+}
+
+//For Anvil:
+//	1. comment out the cbuffer thing and shadered's main function
+//	2. uncomment lines below
+//  3. fix texture fetches
+/*
+struct PS_INPUT {
+    float2 texcoord: TEXCOORD0;
+    float4 color: COLOR0;
+};
+
+struct PS_OUTPUT {
+    float4 color: COLOR0;
+};
+
+float uTime;
+
+PS_OUTPUT main(PS_INPUT input) {
+    PS_OUTPUT output;
+
+    float4 albedo = mainC(float4(input.texcoord,0.0f,0.0f));
+
+    output.color = albedo * input.color;
+
+    return output;
+}
+*/
